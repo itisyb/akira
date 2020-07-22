db125f3a8da2bd62828480e77fb1cc2d944b714d
pandaniell <daniell.7@icloud.com>
2020-07-22 22:35:36 +0200

    Create purge command

---
+++ packages/akira/src/commands/moderation/purge.ts
@@ -0,0 +1,43 @@
+import { Command } from "../../util/registerCommandsAndEvents";
+
+export const command: Command<number> = {
+  description:
+    "Delete up to 100 messages at a time (excluding pinned messages)",
+  aliases: ["delete", "nuke"],
+  usage: "<amount>",
+  examples: ["25", "50", "100"],
+  acceptsArgs: true,
+  requiresArgs: true,
+  clientPermissions: ["MANAGE_MESSAGES", "READ_MESSAGE_HISTORY"],
+  userPermissions: ["MANAGE_MESSAGES"],
+  validateArgs: (_, args) => {
+    const amount = Number(args[0]);
+
+    return isNaN(amount) ? undefined : amount;
+  },
+  async execute(message, amount) {
+    if (amount < 5 || amount > 100) {
+      return message.reply(
+        "please provide at least 5 messages with a maximum of 10"
+      );
+    }
+
+    let messages = await message.channel.messages.fetch({ limit: amount });
+
+    messages = messages.filter((msg) => !msg.pinned);
+
+    try {
+      const collection = await message.channel.bulkDelete(messages, true);
+
+      const response = await message.channel.send(
+        `Successfully deleted \`${collection.size}/${amount}\` message(s), messages older than 14 days cannot be removed by bots`
+      );
+
+      return response.delete({ timeout: 10000 });
+    } catch (error) {
+      return message.channel.send(
+        `Error trying to delete messages:\n\n\`${error.message}\``
+      );
+    }
+  },
+};
