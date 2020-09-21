8d61093d8326e7e8514becaa3f3b21147ce84190
pandaniell <daniell.7@icloud.com>
2020-09-21 20:47:55 +0200

    Fix undefined or null to object conversion error

--- packages/akira/src/middleware/prefixCached.ts
+++ packages/akira/src/middleware/prefixCached.ts
@@ -5,7 +5,7 @@ export const prefixCached = (
   redis: Redis
 ): Middleware<{ prefix?: string }> => async (params, next) => {
   const { model, action, args } = params;
-  const keys = Object.keys(args.select);
+  const keys = Object.keys(args.select ?? {});
   const selectedPrefix = keys.length === 1 && keys.includes("prefix");
   const key = `${args.where.id}:prefix`;
 
