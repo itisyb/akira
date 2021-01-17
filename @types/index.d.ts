b4a999837f4d1f04f9e0834c934ff3fa312d41bc
pandaniell <daniell.wdb@icloud.com>
2021-01-17 18:39:02 +0100

    feat: move elasticsearch options from cli to module

---
+++ @types/index.d.ts
@@ -0,0 +1,4 @@
+declare module "@elastic/ecs-pino-format"
+
+// @TODO: remove when https://github.com/pinojs/pino-elasticsearch/issues/54 has been fixed
+declare module "pino-elasticsearch"
--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,13 +1,28 @@
+import ecsFormat from "@elastic/ecs-pino-format"
 import pino from "pino"
+import pinoElastic from "pino-elasticsearch"
 import { __DEV__ } from "../constants"
 
-export const logger = pino({
-  name: process.env.npm_package_name,
-  ...(__DEV__ && {
-    prettyPrint: {
-      colorize: true,
-      ignore: "hostname,pid",
-      translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
-    },
-  }),
-})
+ecsFormat()
+
+const streamToElastic = () => {
+  return pinoElastic({
+    consistency: "one",
+    node: process.env.SEARCHBOX_URL,
+    "es-version": 7,
+  })
+}
+
+const prettyPrint = {
+  colorize: true,
+  ignore: "hostname,pid",
+  translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
+}
+
+export const logger = pino(
+  {
+    name: process.env.npm_package_name,
+    ...(__DEV__ ? { prettyPrint } : ecsFormat),
+  },
+  !__DEV__ && streamToElastic()
+)
