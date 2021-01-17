31881e2d6075964136586b3e0c558526ea83ac18
pandaniell <daniell.wdb@icloud.com>
2021-01-17 17:17:09 +0100

    ci: get searchbox url from heroku config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node dist/index.js 2>&1 | pino-elasticsearch --node $SEARCHBOX_URL
+node dist/index.js 2>&1 | pino-elasticsearch --node $(heroku config:get SEARCHBOX_URL -a akira-bot-discord)
